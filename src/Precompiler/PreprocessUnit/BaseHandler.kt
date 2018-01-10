793815b511c36bb31978e7a1354a262ed4817e91
laujangit@gmail.com <1urmygithub>
2018-01-10 11:25:07 +0800

    aaa

---
+++ src/Precompiler/PreprocessUnit/BaseHandler.kt
@@ -0,0 +1,32 @@
+package Precompiler.PreprocessUnit
+
+/**
+ * Created by lauya on 2018/1/8.
+ */
+abstract class BaseHandler{
+    abstract var Code:String;
+    abstract val handlerType:Int;
+    abstract fun putChar(chr:Char):Unit;
+    abstract var new_handler_chr: Char;
+    abstract fun getNewHandler(): BaseHandler;
+    abstract var shouldSwitch:Boolean;
+
+    fun shouldSwitch():Boolean{
+        return shouldSwitch;
+    };
+
+    fun getProcessedCode(): String {
+        return this.Code;
+    }
+
+//    fun aaa():Unit{
+//        var typeStr=when(handlerType){
+//            1->"PrecompileCommandHandler"
+//            2->"CommentCodeHandler"
+//            3->"CSourceCodeHandler"
+//            4->"StringHandler"
+//            else->"unknown"
+//        }
+//        print(typeStr+"\n");
+//    }
+}
